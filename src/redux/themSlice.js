4e78a499b644b746904e0732f0a0dcc21afa2adc
ahmedmeid99 <you@example.com>
2022-11-11 11:35:35 +0200

    fix-4

--- src/redux/themSlice.js
+++ src/redux/themSlice.js
@@ -1,25 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit"
+// import { createSlice } from "@reduxjs/toolkit"
 
-export const usersSlice = createSlice({
-    name: 'users',
-    initialState: { userType: '', isLogedin: false },
-    reducers: {
-        setAsAdmin(state) {
-            state.userType = 'admin'
-            state.isLogedin = true
-            localStorage.setItem('login', "admin")
-        },
-        setAsUser(state) {
-            state.userType = 'user'
-            state.isLogedin = true
-            localStorage.setItem('login', "user")
-        },
-        reset() {
-            return { userType: '', isLogedin: false }
-        }
-    }
-})
+// export const usersSlice = createSlice({
+//     name: 'users',
+//     initialState: { userType: '', isLogedin: false },
+//     reducers: {
+//         setAsAdmin(state) {
+//             state.userType = 'admin'
+//             state.isLogedin = true
+//             localStorage.setItem('login', "admin")
+//         },
+//         setAsUser(state) {
+//             state.userType = 'user'
+//             state.isLogedin = true
+//             localStorage.setItem('login', "user")
+//         },
+//         reset() {
+//             return { userType: '', isLogedin: false }
+//         }
+//     }
+// })
 
-const usersActions = usersSlice.actions
+// const usersActions = usersSlice.actions
 
-export default usersActions
+// export default usersActions
