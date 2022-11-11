379efb18f3c9d37e31cc8f16d0124a02224d3582
ahmedmeid99 <you@example.com>
2022-11-11 11:22:12 +0200

    fix-2

--- src/redux/projectsSlice.js
+++ src/redux/projectsSlice.js
@@ -1,21 +1,19 @@
 // import { Provider, useSelector, useDispatch } from "react-redux"
 import { createSlice, configureStore } from "@reduxjs/toolkit"
-
-// POST project
-const addOne = async (project) => {
-    const responsive = await fetch('https://react-http-f4f9d-default-rtdb.firebaseio.com/portfolioprojects.json', {
-        method: 'POST',
-        body: JSON.stringify(project),
-        headers: {
-            'Contect-Type': 'application/json',
-        }
-    })
-    const data = await responsive.json()
-    console.log(data)
-}
+import shuffle1 from '../../assets/images/shuffle-01.jpg'
+import shuffle2 from '../../assets/images/shuffle-02.jpg'
+import shuffle3 from '../../assets/images/shuffle-03.jpg'
+import shuffle4 from '../../assets/images/shuffle-04.jpg'
+import shuffle5 from '../../assets/images/shuffle-05.jpg'
+import shuffle6 from '../../assets/images/shuffle-06.jpg'
+import shuffle7 from '../../assets/images/shuffle-07.jpg'
+import shuffle8 from '../../assets/images/shuffle-08.jpg'
+const projects = [
+    { imgLink: shuffle1, title: 'Awesome Image', type: 'Potography' },
+]
 export const projectsSlice = createSlice({
     name: 'projects',
-    initialState: { projects: [], filtered: [] },
+    initialState: { projects: projects, filtered: [] },
     reducers: {
         setProjects(state, action) {
             state.projects = action.payload
